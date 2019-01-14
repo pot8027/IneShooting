8b13a573c4443483656c3e93484859f2cca4d942
a_sugino <akiyoshisugino827@gmail.com>
2019-01-14 14:37:18 +0900

    王さん実装

--- Assets/Scripts/Token/Enemy/Enemy1.cs
+++ Assets/Scripts/Token/Enemy/Enemy1.cs
@@ -9,21 +9,21 @@ public class Enemy1 : Enemy
     /// <returns>The pdate1.</returns>
     IEnumerator IEUpdate1()
     {
-        SetVelocity(180, 3);
+        SetVelocity(180, 3.0f);
 
         while (true)
         {
             float dir = GetTargetAim();
-            EnemyShot2.Add(X, Y, dir, 2);
+            EnemyShot1.Add(X, Y, dir, 2);
 
-            // プレイヤーと一定距離以上の場合はコルーチン２
-            if (GetTargetDistance() > 4)
-            {
-                SetCoroutinueID(2);
-                break;
-            }
+            //// プレイヤーと一定距離以上の場合はコルーチン２
+            //if (GetTargetDistance() > 4)
+            //{
+            //    SetCoroutinueID(2);
+            //    break;
+            //}
 
-            yield return new WaitForSeconds(2.0f);
+            yield return new WaitForSeconds(2.5f);
         }
     }
 
@@ -45,7 +45,7 @@ public class Enemy1 : Enemy
                 break;
             }
 
-            yield return new WaitForSeconds(1.0f);
+            yield return new WaitForSeconds(5.0f);
         }
     }
 }
---
+++ Assets/Scripts/Token/Enemy/Wan.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wan : Enemy
+{
+    /// <summary>
+    /// コルーチン１
+    /// </summary>
+    /// <returns>The pdate1.</returns>
+    IEnumerator IEUpdate1()
+    {
+        SetVelocity(180, 2);
+        yield return new WaitForSeconds(1.5f);
+        SetVelocity(0, 0);
+
+        while (true)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                ShotCircle();
+                yield return new WaitForSeconds(1.0f);
+            }
+
+            yield return new WaitForSeconds(3.0f);
+        }
+    }
+
+    private void ShotCircle()
+    {
+        for (int i = 0; i < 24; i++)
+        {
+            EnemyShot1.Add(X, Y, i * 15, 3);
+        }
+    }
+}
